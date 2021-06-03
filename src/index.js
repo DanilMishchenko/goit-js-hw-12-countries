2c3a5850b5867855079776b064ecff07520d9907
Danil Mishchenko <dan5679mi@gmail.com>
2021-06-03 23:04:42 +0300

    Добавлен функционал

--- src/index.js
+++ src/index.js
@@ -1,1 +1,71 @@
-import './sass/main.scss';
+import './sass/styles.scss';
+import countryCardTmpl from './templates/country-cards.hbs';
+import countryListTmpl from './templates/several-countries.hbs';
+import API from './js/fetchCountries';
+import getRefs from './js/get-refs';
+import debounce from 'lodash.debounce';
+import { alert, Stack } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const refs = getRefs();
+
+refs.input.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch(e) {
+    refs.cardContainer.innerHTML = "";
+    const searchQuery = e.target.value;
+    if (searchQuery === "") {
+        return;
+    }
+    API.fetchCountries(searchQuery)
+        .then(searchCheck)
+        .catch(onFetchError);
+};
+
+function searchCheck(country) {
+    if (country.length === 1) {
+        renderCountryCard(country);
+        return;
+    };
+            
+    if (country.length <= 10 && country.length > 1) {
+        renderCountryList(country);
+        return;
+    };
+
+    if (country.length > 10) {
+        alert({
+            text: "Too many matches found. Please enter a more specific query!",
+            hide: true,
+            delay: 500,
+            styling: 'brighttheme',
+            // stack: new Stack({
+            //     maxOpen: 1,
+            //     dir1: 'up',
+            //     dir2: 'right',
+            //     firstpos1: 100,
+            //     firstpos2: 100,
+            //     context: document.querySelector('body'),
+            // })
+        });
+    return;
+    };
+};
+
+function renderCountryCard(country) {
+    const markupCard = countryCardTmpl(country);
+    refs.cardContainer.innerHTML = markupCard;
+};
+
+function renderCountryList(country) {
+    const markupList = countryListTmpl(country);
+    refs.cardContainer.innerHTML = markupList;
+};
+
+function onFetchError(err) {
+    alert({
+        text: `Шопопало`,
+        hide: true,
+        delay: 1000,
+    });
+};
